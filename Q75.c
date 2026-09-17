d3fe583443cc6265c6b86f491b67120b21357572
arihantchauhan90 <arihantchauhan90@gmail.com>
2026-09-17 08:24:06 +0530

    Add files via upload

---
+++ Q75.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    int A[r][c], B[r][c], sum[r][c];
+
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    // Adding the matrices
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    printf("Sum of the matrices:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
